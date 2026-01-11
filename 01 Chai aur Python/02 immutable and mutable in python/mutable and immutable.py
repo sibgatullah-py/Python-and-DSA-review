49f2a1e1cf10863e86e38afedc3fa06ff56f26dd
sibgatullah-dev <sibgatullah881@gmail.com>
2026-01-12 02:04:02 +0600

    defination of mutable and immutable

---
+++ 01 Chai aur Python/02 immutable and mutable in python/mutable and immutable.py
@@ -0,0 +1,24 @@
+'''
+Mutable and Immutable datatypes in python 
+
+There are two types of data in python
+    1. Mutable
+    2. Immutable
+    
+# Mutable:->
+    . List
+    . Set 
+    . Dictionary
+    . Bytearray
+    . Array
+    
+# Immutable:->
+    . Integers
+    . Floating-point numbers
+    . Boolean
+    . Strings
+    . Tuples
+    . Frozen set
+    . Bytes
+    
+'''
