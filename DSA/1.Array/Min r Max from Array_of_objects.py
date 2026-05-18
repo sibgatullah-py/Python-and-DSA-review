d8e1719beab1b8684f3b0e89b188b48292a3c15b
sibgatullah-dev <sibgatullah881@gmail.com>
2026-05-18 06:03:12 +0600

    assertion error and proper file handling

--- DSA/1.Array/Min r Max from Array_of_objects.py
+++ DSA/1.Array/Min r Max from Array_of_objects.py
@@ -1,26 +1,42 @@
 import csv
 
-class Student:
-    def __init__(self, name, roll, mark):
-        self.name = name
-        self.roll = roll
-        self.mark = mark
+print("Put 0 if you want to read the file . Put 1 if you want to write in the file")
+rotation = int(input())
+
+if rotation == 1:
+    class Student:
+        def __init__(self, name, roll, mark):
+            self.name = name
+            self.roll = roll
+            self.mark = mark
+            
+
+    students = []
+
+    number = int(input("How many students: "))
+    for s in range(number):
+        name = input("Name: ")
+        roll = int(input("Roll: "))
+        mark = int(input("Mark: "))
         
-
-students = []
-
-number = int(input("How many students: "))
-for s in range(number):
-    name = input("Name: ")
-    roll = int(input("Roll: "))
-    mark = int(input("Mark: "))
-    
-    students.append(Student(name, roll, mark))
-    
-students = sorted(students, key=lambda s: s.mark, reverse=True)
-    
-with open('students.csv', 'a', newline='') as file:
-    write = csv.DictWriter(file, fieldnames=['name','roll','mark'])
-    write.writerow({'name':name,'roll':roll,'mark':mark})
-    for s in students:
-        write.writerow({'name':s.name,'roll':s.roll,'mark':s.mark})
+        students.append(Student(name, roll, mark))
+        
+    students = sorted(students, key=lambda s: s.mark, reverse=True)
+        
+    with open('students.csv', 'a', newline='') as file:
+        write = csv.DictWriter(file, fieldnames=['name','roll','mark'])
+        write.writerow({'name':name,'roll':roll,'mark':mark})
+        
+        
+elif rotation == 0:
+    info = []
+    with open('students.csv') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            info.append({"name":row['name'], "roll":row['roll'], "mark":row['mark']})
+            
+    for student in info:
+        print(student['name'], student['roll'], student['mark'])
+        
+else:
+    assert False, "Please enter number between 1 or 0. INVALID INPUT!!"
