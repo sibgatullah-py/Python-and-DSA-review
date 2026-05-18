4894178af079cc503ba4a3ab6134d68b9d8d1c34
sibgatullah-dev <sibgatullah881@gmail.com>
2026-05-18 23:07:14 +0600

    min max sorting array object and file management

--- DSA/1.Array/Min r Max from Array_of_objects.py
+++ DSA/1.Array/Min r Max from Array_of_objects.py
@@ -3,13 +3,13 @@ import csv
 print("Put 0 if you want to read the file . Put 1 if you want to write in the file")
 rotation = int(input())
 
-if rotation == 1:
-    class Student:
-        def __init__(self, name, roll, mark):
-            self.name = name
-            self.roll = roll
-            self.mark = mark
+class Student:
+    def __init__(self, name, roll, mark):
+        self.name = name
+        self.roll = roll
+        self.mark = mark
             
+if rotation == 1:
 
     students = []
 
@@ -21,11 +21,16 @@ if rotation == 1:
         
         students.append(Student(name, roll, mark))
         
-    students = sorted(students, key=lambda s: s.mark, reverse=True)
+    students = sorted(students, key=lambda s: s.roll)
         
     with open('students.csv', 'a', newline='') as file:
         write = csv.DictWriter(file, fieldnames=['name','roll','mark'])
-        write.writerow({'name':name,'roll':roll,'mark':mark})
+        for s in students:
+            write.writerow({
+                'name': s.name,
+                'roll': s.roll,
+                'mark': s.mark
+            })
         
         
 elif rotation == 0:
@@ -37,6 +42,18 @@ elif rotation == 0:
             
     for student in info:
         print(student['name'], student['roll'], student['mark'])
+        
+    total = len(info)
+      
+    if total > 1:
+        mn = min(info, key= lambda x: x['mark'])
+        mx = max(info, key= lambda x: x['mark'])
+        print(f"Lowest: {mn['name']}, {mn['roll']}, {mn['mark']}")
+        print(f"Highest: {mx['name']}, {mx['roll']}, {mx['mark']}")
+    elif total == 1:
+        print(f"Highest: {info[0]['name']}, {info[0]['roll']}, {info[0]['mark']}")
+    else:
+        print("No student data found..")
         
 else:
     assert False, "Please enter number between 1 or 0. INVALID INPUT!!"
